6c0bccf0dcf4758f35447dfa24c35cf454166da9
Juston Hart <contact@justonh.art>
2024-07-08 08:32:08 -0500

    Declare base classes

---
+++ LinkedList.js
@@ -0,0 +1,52 @@
+import Node from "Node"
+export default class LinkedList {
+	head = null;
+
+	append(value) {
+
+	}
+
+	prepend(value) {
+
+	}
+
+	size() {
+
+	}
+
+	head() {
+
+	}
+
+	tail() {
+
+	}
+
+	at(index) {
+
+	}
+
+	pop() {
+
+	}
+
+	contains(value) {
+
+	}
+
+	find(value) {
+
+	}
+
+	toString() {
+
+	}
+
+	insertAt(value, index) {
+
+	}
+
+	removeAt(index) {
+
+	}
+}
---
+++ Node.js
@@ -0,0 +1,4 @@
+export default class Node {
+	value = null;
+	nextNode = null;
+}
